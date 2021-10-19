94f0ab4326eceb56982141508de1b71a3f681984
Leonardo Lenzi <l.lenzi@luisaviaroma.com>
2021-10-19 14:17:09 +0200

    sistemo il fixed anche per il popup di export

--- components/popup/index.js
+++ components/popup/index.js
@@ -6,12 +6,12 @@ const PopUp = props => {
 
     return (
         <Portal>
-            <>
+            <div className="fixed inset-0">
                 <div className="overlay h-full w-full bg-black opacity-50 absolute inset-0 z-20" />
                 <div className="absolute rounded inset-0 flex flex-col align-middle justify-center content-center h-full z-30">
                     {children}
                 </div>
-            </>
+            </div>
         </Portal>
     )
 }
